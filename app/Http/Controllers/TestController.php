1e518c2fe5a7e8554fc7a0ee9aa8310dfdd76cd4
freedog1 <koki375656@gmail.com>
2020-11-15 23:33:40 +0900

    コントローラー

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Test;
-use Illuminate\support\facades\DB;
+
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {

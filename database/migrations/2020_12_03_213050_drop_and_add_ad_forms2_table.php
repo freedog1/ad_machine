9052ec920fc5ab873555b055ff045004cda0bfe2
freedog1 <koki375656@gmail.com>
2020-12-03 21:43:15 +0900

    dbカラム修正

---
+++ database/migrations/2020_12_03_213050_drop_and_add_ad_forms2_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropAndAddAdForms2Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ad_forms', function (Blueprint $table) {
+            //
+            $table->string('status');
+            $table->integer('users_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ad_forms', function (Blueprint $table) {
+            //
+        });
+    }
+}

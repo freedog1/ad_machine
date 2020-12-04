e6488d4e2c382baac10093e30f23163ea19fe5a4
freedog1 <koki375656@gmail.com>
2020-12-04 09:12:59 +0900

    画面作成

--- resources/views/ad/index.blade.php
+++ resources/views/ad/index.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.app')
 
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">広告一覧
+          
+          <div class="card-body">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+@endsection
+
 ・広告掲載申請はこちら
 
-・ユーザー名
 ・広告名(ad_name)
 ・画像
 

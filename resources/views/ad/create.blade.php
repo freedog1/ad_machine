00bce033f65835006882da8e6acd7951b1b2f96f
freedog1 <koki375656@gmail.com>
2020-12-07 20:02:12 +0900

    フォーム作成

--- resources/views/ad/create.blade.php
+++ resources/views/ad/create.blade.php
@@ -5,7 +5,7 @@
   <div class="row justify-content-center">
     <div class="col-md-12">
       <div class="card">
-        <div class="card-header">広告一覧
+        <div class="card-header">広告新規登録
           
           <div class="card-body">
             @if (session('status'))
@@ -14,7 +14,34 @@
               </div>
             @endif
 
-            <form method="GET" action="{{ route('ad.create') }}">
+            {{-- エラー出力 --}}
+            @if ($errors->any())
+            <div class="alert alert-denger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            @endif
+
+            <form method="POST" action="{{ route('ads.store') }}">
+              @csrf
+              広告名
+              <input type="text" name="ad_name">
+              <br>
+              対象性別
+              <input type="radio" name="gender" value="0">男性</input>
+              <input type="radio" name="gender" value="1">女性</input>
+              <br>
+              対象年齢
+              <select name="age">
+                <option value="">洗濯してください</option>
+                <option value="1">~19歳</option>
+                
+              対象地域
+
+              
+
               <button type="submit" class="btn btn-primary">
                 新規登録
               </button>
